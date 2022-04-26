3bc306e414751c3a221aa9f6b9cb4c7227231487
Dasha <rogozyanchik@gmail.com>
2022-04-26 18:23:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// function switchDivs() {
+//
+//         let divParent = document.querySelector('#toChangeChild');
+//         let firstDiv = divParent.firstChild;
+//         let newDiv = firstDiv.cloneNode(true);
+//         divParent.appendChild(newDiv);
+//         divParent.removeChild(firstDiv)
+//         console.log(newDiv);
+//
+// }
+
+// const mmObj = window.matchMedia("(max-width: 767px)")
+// switchDivs(mmObj)
